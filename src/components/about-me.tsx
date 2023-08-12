0f3a4320efe0733c40d98e7239e9d3c9e8e91759
Teresa <tqwu@ucsc.edu>
2023-08-12 00:15:57 -0700

    #4 Modified format for about-me

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -5,8 +5,20 @@ import profile from '../../public/profile.png'
 export default function AboutMe() {
   return (
     <div className="py-3 sm:py-12 grid place-items-center">
-      <div className="grid grid-cols-7 grid-rows-4 sm:grid-rows-1 max-w-3xl text-sm sm:text-base font-normal leading-4 sm:leading-6 text-amber-950">
-        <Image className="col-span-3 row-span-3 sm:row-span-1 justify-self-center self-center px-2 lg:p-0" src={profile} height={300} alt="Picture of Teresa Wu" />
+      <div className="grid sm:grid-cols-7 sm:grid-rows-1 max-w-3xl text-base font-normal leading-6 text-amber-950">
+
+        {/* Mobile text start */}
+        <div className="block sm:hidden pt-4 px-2 text-center">
+          <div className="pb-2">
+            My name is Teresa, and I&apos;m a rising senior at the University of California, Santa Cruz, studying Computer Science (BS) and History of Consciousness.
+          </div>
+          <div className="pb-2">
+            I currently work as a Full Stack Web Development Intern at Think Round Inc..
+          </div>
+        </div>
+        {/* Mobile text end */}
+
+        <Image className="sm:col-span-3 sm:row-span-1 justify-self-center self-center px-2 lg:p-0" src={profile} height={300} alt="Picture of Teresa Wu" />
 
         {/* Desktop paragraph start */}
         <div className="hidden sm:grid col-span-4 gap-5 pt-2 pr-7">
@@ -25,24 +37,16 @@ export default function AboutMe() {
         </div>
         {/* Desktop paragraph end */}
 
-        {/* Mobile paragraph start */}
-        <div className="sm:hidden col-span-4 row-span-3 pt-4 pr-2 self-center">
-          <div className="pb-2">
-            My name is Teresa, and I&apos;m a rising senior at the University of California, Santa Cruz, studying Computer Science (BS) and History of Consciousness.
-          </div>
-          <div className="pb-2">
-            I currently work as a Full Stack Web Development Intern at Think Round Inc..
-          </div>
+        {/* Mobile text start */}
+        <div className="block sm:hidden pb-3 pt-2 px-2 text-center">
           <div className="pb-2">
             I aspire to continue to build efficient applications that provide optimal user experiences.
           </div>
-        </div>
-        <div className="sm:hidden col-span-7 row-span-1 pl-3 pr-2 self-start text-center">
           <div>
             Check out my LinkedIn to learn more about me, or explore my GitHub to see some of my projects! 
           </div>
         </div>
-        {/* Mobile paragraph end */}
+        {/* Mobile text end */}
 
       </div>
     </div>
