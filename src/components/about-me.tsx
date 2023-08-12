d7fb12abbeea5508649a755ca5f0e55cbe1359d7
Teresa <tqwu@ucsc.edu>
2023-08-11 22:56:32 -0700

    #4 Modified mobile responsiveness about-me

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -5,8 +5,8 @@ import profile from '../../public/profile.png'
 export default function AboutMe() {
   return (
     <div className="py-3 sm:py-12 grid place-items-center">
-      <div className="grid grid-cols-7 grid-rows-3 sm:grid-rows-1 max-w-3xl text-xs sm:text-base font-normal leading-4 sm:leading-6 text-amber-950">
-        <Image className="col-span-2 sm:col-span-3 row-span-2 sm:row-span-1 justify-self-center self-center px-2 lg:p-0" src={profile} height={300} alt="Picture of Teresa Wu" />
+      <div className="grid grid-cols-7 grid-rows-4 sm:grid-rows-1 max-w-3xl text-xs sm:text-base font-normal leading-4 sm:leading-6 text-amber-950">
+        <Image className="col-span-3 row-span-3 sm:row-span-1 justify-self-center self-center px-2 lg:p-0" src={profile} height={300} alt="Picture of Teresa Wu" />
 
         {/* Desktop paragraph start */}
         <div className="hidden sm:grid col-span-4 gap-5 pt-2 pr-7">
@@ -26,18 +26,18 @@ export default function AboutMe() {
         {/* Desktop paragraph end */}
 
         {/* Mobile paragraph start */}
-        <div className="sm:hidden col-span-5 row-span-2 pt-4 pr-2 self-center">
+        <div className="sm:hidden col-span-4 row-span-3 pt-4 pr-2 self-center">
           <div className="pb-2">
             My name is Teresa, and I&apos;m a rising senior at the University of California, Santa Cruz, studying Computer Science (BS) and History of Consciousness.
           </div>
           <div className="pb-2">
             I currently work as a Full Stack Web Development Intern at Think Round Inc..
           </div>
-        </div>
-        <div className="sm:hidden col-span-7 row-span-1 pl-3 pr-2 self-center">
           <div className="pb-2">
             I aspire to continue to build efficient applications that provide optimal user experiences.
           </div>
+        </div>
+        <div className="sm:hidden col-span-7 row-span-1 pl-3 pr-2 self-start text-center">
           <div>
             Check out my LinkedIn to learn more about me, or explore my GitHub to see some of my projects! 
           </div>
