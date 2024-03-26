3029d2ec2efce016f3e7961eb458d7a679b7f1f1
Teresa <tqwu@ucsc.edu>
2024-03-26 15:30:34 -0700

    #12 Adding typing animation to mobile view

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -14,7 +14,6 @@ export default function Intro() {
           <div className="hidden sm:columns-2 sm:pt-16 sm:flex sm:flex-initial sm:justify-center">
             <div className="pt-14 pl-8 pr-10 text-center text-3xl font-semibold leading-9 text-slate-200">
               <div className="pb-4 text-4xl">Hi! I&apos;m Teresa Wu</div>
-              {/* <div>I turn coffee into code for the web.</div> */}
               <IntroAnim />
             </div>
             <Image className="pr-8" src={avatar} width={230} alt="Avatar of Teresa Wu" priority />
@@ -25,7 +24,7 @@ export default function Intro() {
         <div className="pb-5 grid grid-rows-5 grid-cols-3 items-center content-center">
           <div className="px-2 row-span-2 col-span-3 sm:hidden text-center text-2xl font-semibold leading-7 text-slate-200">
             <div className="pb-4">Hi! I&apos;m Teresa Wu</div>
-            <div>I turn coffee into code for the web.</div>
+            <IntroAnim />
           </div>
           <div className="col-span-1"/>
           <Image className="pr-5 row-span-3 col-span-2 justify-self-end sm:hidden" src={avatar_mobile} width={150} alt="Avatar of Teresa Wu" priority />
