05f1047b686522011482efe2d1e1e2975cbd8fdf
Teresa <tqwu@ucsc.edu>
2024-03-26 22:24:50 -0700

    #13 Formatted experiences section

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -62,152 +62,115 @@ const experiences = [
 export default function Experience() {
 
   return (
-    <div className="px-5 text-slate-200 font-mono">
+    <div className="px-5 text-slate-200 font-mono grid grid-cols-6">
 
-      <div className="text-3xl">Experiences</div>
+      <div className="text-5xl col-span-6 place-self-center">Experiences</div>
 
-      <div className="">
+      <div className="col-span-1"/>
+      <div className="col-span-3 justify-self-end w-full">
         {experiences.map((experience) => (
-          <div key={experience.title} className="border-2 rounded-xl border-slate-500 p-2 my-2">
-            <div>{experience.title}</div>
-            <div>{experience.location}</div>
-            <div>{experience.dates}</div>
-            <ul className="text-sm list-disc px-6">
+          <div key={experience.title} className="grid gap-1 grid-cols-3 border-2 rounded-xl border-slate-500 py-6 px-3 my-12 my-2">
+            <div className="col-span-2 pl-2">
+              <div className="text-2xl pb-2">{experience.title}</div>
+              <div>{experience.location}</div>
+              <div>{experience.dates}</div>
+            </div>
+            <div className="col-span-1 p-1.5 mr-3 w-64 grid justify-self-end items-center">
+              <Image
+                className=""
+                src={experience.logo}
+                height={500}
+                width={350}
+                alt="Avatar of Teresa Wu"
+                priority
+              />
+            </div>
+            <ul className="text-sm list-disc px-6 pt-2 col-span-3">
               {experience.description.map((item, index) => (
                 <li key={index} className="py-0.5">
                   {item}
                 </li>
               ))}
             </ul>
-            <div className="bg-slate-100/90 p-2 rounded-xl w-64 grid place-self-center">
-              <Image
-                className=""
-                src={experience.logo}
-                height={400}
-                width={250}
-                alt="Avatar of Teresa Wu"
-                priority
-              />
-            </div>
           </div>
         ))}
       </div>
 
-
-      <Stepper
-        orientation="vertical"
-        sx={{
-          '--Stepper-verticalGap': '2.5rem',
-          '--StepIndicator-size': '2.5rem',
-          '--Step-gap': '1rem',
-          '--Step-connectorInset': '0.5rem',
-          '--Step-connectorRadius': '1rem',
-          '--Step-connectorThickness': '4px',
-          '--joy-palette-success-solidBg': 'var(--joy-palette-success-400)',
-          [`& .${stepClasses.completed}`]: {
-            '&::after': { bgcolor: 'success.solidBg' },
-          },
-          [`& .${stepClasses.active}`]: {
-            [`& .${stepIndicatorClasses.root}`]: {
-              border: '4px solid',
-              borderColor: '#fff',
-              boxShadow: (theme) => `0 0 0 1px ${theme.vars.palette.primary[500]}`,
+      <div className="col-span-1 justify-self-start my-12 pl-24">
+        <Stepper
+          orientation="vertical"
+          sx={{
+            '--Stepper-verticalGap': '2.5rem',
+            '--StepIndicator-size': '2.5rem',
+            '--Step-gap': '1rem',
+            '--Step-connectorInset': '0.5rem',
+            '--Step-connectorRadius': '1rem',
+            '--Step-connectorThickness': '4px',
+            '--joy-palette-success-solidBg': 'var(--joy-palette-success-400)',
+            [`& .${stepClasses.completed}`]: {
+              '&::after': { bgcolor: 'success.solidBg' },
             },
-          },
-          [`& .${stepClasses.disabled} *`]: {
-            color: 'neutral.softDisabledColor',
-          },
-          [`& .${typographyClasses['title-sm']}`]: {
-            textTransform: 'uppercase',
-            letterSpacing: '1px',
-            fontSize: '10px',
-          },
-        }}
-      >
-        <Step
-          completed
-          indicator={
-            <StepIndicator variant="solid" color="success">
-            </StepIndicator>
-          }
+            [`& .${stepClasses.active}`]: {
+              [`& .${stepIndicatorClasses.root}`]: {
+                border: '4px solid',
+                borderColor: '#fff',
+                boxShadow: (theme) => `0 0 0 1px ${theme.vars.palette.primary[500]}`,
+              },
+            },
+            [`& .${stepClasses.disabled} *`]: {
+              color: 'neutral.softDisabledColor',
+            },
+            [`& .${typographyClasses['title-sm']}`]: {
+              textTransform: 'uppercase',
+              letterSpacing: '1px',
+              fontSize: '10px',
+            },
+          }}
         >
-          <div>
-            <Typography level="title-sm">Step 1</Typography>
-            Basic Details
-          </div>
-        </Step>
-        <Step
-          completed
-          indicator={
-            <StepIndicator variant="solid" color="success">
-            </StepIndicator>
-          }
-        >
-          <div>
-            <Typography level="title-sm">Step 2</Typography>
-            Company Details
-          </div>
-        </Step>
-        <Step
-          active
-          indicator={
-            <StepIndicator variant="solid" color="primary">
-            </StepIndicator>
-          }
-        >
-          <div>
-            <Typography level="title-sm">Step 3</Typography>
-            Subscription plan
-          </div>
-        </Step>
-        <Step disabled indicator={<StepIndicator>3</StepIndicator>}>
-          <div>
-            <Typography level="title-sm">Step 4</Typography>
-            Payment details
-          </div>
-        </Step>
-      </Stepper>
+          <Step
+            completed
+            indicator={
+              <StepIndicator variant="solid" color="success">
+              </StepIndicator>
+            }
+          >
+            <div>
+              <Typography level="title-sm">Step 1</Typography>
+              Basic Details
+            </div>
+          </Step>
+          <Step
+            completed
+            indicator={
+              <StepIndicator variant="solid" color="success">
+              </StepIndicator>
+            }
+          >
+            <div>
+              <Typography level="title-sm">Step 2</Typography>
+              Company Details
+            </div>
+          </Step>
+          <Step
+            active
+            indicator={
+              <StepIndicator variant="solid" color="primary">
+              </StepIndicator>
+            }
+          >
+            <div>
+              <Typography level="title-sm">Step 3</Typography>
+              Subscription plan
+            </div>
+          </Step>
+          <Step disabled indicator={<StepIndicator>3</StepIndicator>}>
+            <div>
+              <Typography level="title-sm">Step 4</Typography>
+              Payment details
+            </div>
+          </Step>
+        </Stepper>
+      </div>
     </div>
-    // <div className="py-3 sm:py-12 grid place-items-center">
-    //   <div className="grid sm:grid-cols-7 sm:grid-rows-3 max-w-2xl text-base font-normal leading-6 text-slate-200">
-    //     <Tab.Group vertical>
-    //       <Tab.List className="sm:col-span-2 sm:row-span-3 flex overflow-x-auto sm:overflow-hidden px-2 sm:pr-0 pb-2 sm:pb-0">
-    //         <div className="flex sm:flex-col sm:gap-7">
-    //           {experiences.map((experience) => (
-    //             <Tab as={Fragment} key={experience.name}>
-    //               {({ selected }) => (
-    //                 <span
-    //                   className={`${selected ? "bg-neon-purple/80" : "bg-neon-pink/80"}
-    //                   outline-amber-900/50 rounded-3xl px-3 py-1 m-1 w-max sm:w-auto`}
-    //                 >
-    //                   <button>{experience.name}</button>
-    //                 </span>
-    //               )}
-    //             </Tab>
-    //           ))}
-    //         </div>
-    //       </Tab.List>
-    //       <Tab.Panels className="sm:col-span-5 sm:row-span-3 bg-neon-pink/70 rounded-3xl h-fit min-h-custom sm:min-h-0 sm:h-60 py-3 px-5 mx-3 sm:ml-0">
-    //         {experiences.map((experience) => (
-    //           <Tab.Panel key={experience.company}>
-    //             <span className="pb-2">{experience.title}</span>
-    //             <span className="text-amber-900/70 pb-2">
-    //               {" "}
-    //               at {experience.company}
-    //             </span>
-    //             <div className="text-sm pb-2">{experience.dates}</div>
-    //             <ul className="text-sm list-disc px-4">
-    //               {experience.description.map((item, index) => (
-    //                 <li key={index} className="p-1">
-    //                   {item}
-    //                 </li>
-    //               ))}
-    //             </ul>
-    //           </Tab.Panel>
-    //         ))}
-    //       </Tab.Panels>
-    //     </Tab.Group>
-    //   </div>
-    // </div>
   );
 }
