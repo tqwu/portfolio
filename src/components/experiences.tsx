f4fd94377a28fe4ba2df576c76d428b05f6e1084
Teresa <tqwu@ucsc.edu>
2024-04-01 16:42:47 -0700

    #13 Adjusting experience attributes and fixing descriptions

--- src/components/experiences.tsx
+++ src/components/experiences.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 const experiences = [
   {
-    title: `Software Engineering Intern`,
+    header: `Software Engineering Intern`,
+    subheader: `Intel RDT, Docker, Prometheus, Grafana, Go`,
     company: `Nutanix`,
     logo: `/Nutanix.png`,
     location: `San Jose, CA`,
     dates: `Dec 2023 - Present`,
-    technologies: `Intel RDT, Docker, Prometheus, Grafana, Go`,
     description: [
       `Developed an end-to-end memory monitoring system to gain analytical insight into Nutanix cloud infrastructure`,
       `Implemented a data exporter designed to gather metrics from 6000+ virtual machines`,
@@ -16,36 +16,36 @@ const experiences = [
     ],
   },
   {
-    title: `Engineering Tutor`,
+    header: `Engineering Tutor`,
+    subheader: `Computer Systems Design in C`,
     company: `University of California, Santa Cruz`,
     logo: `/Baskin.png`,
     location: `Santa Cruz, CA`,
     dates: `Dec 2023 - Present`,
-    technologies: ``,
     description: [
       `Instructed 150+ students via weekly tutoring sessions`,
       `Reviewed course material and assisted students with troubleshooting and debugging programming assignments`,
     ],
   },
   {
-    title: `Full Stack Developer Intern`,
+    header: `Full Stack Developer Intern`,
+    subheader: `React, Node.js, Next.js, PostgreSQL, Tailwind CSS, TypeScript`,
     company: `Think Round, Inc.`,
     logo: `/ThinkRound.png`,
     location: `San Francisco, CA`,
     dates: `Jun 2023 - Sep 2023`,
-    technologies: `React, Node.js, Next.js, PostgreSQL, Tailwind CSS, TypeScript`,
     description: [
       `Created a website that features the latest Think Round exhibitions and spotlights mental health/wellness resources, developed with industry-standard UI/UX and accessibility practices`,
       `Presented a live demo of version 1.0 at a Grand Launch unveiling to an audience of 100+ attendees`,
     ],
   },
   {
-    title: `Lead Advisor & Editor-in-Chief`,
+    header: `Lead Advisor & Editor-in-Chief`,
+    subheader: `Crown College Advising`,
     company: `University of California, Santa Cruz`,
     logo: `/UCSC.png`,
     location: `Santa Cruz, CA`,
     dates: `September 2022 - Present`,
-    technologies: ``,
     description: [
       `Supervised and trained a team of 5 academic peer advisors`,
       `Organized 20+ successful student outreach events`,
@@ -65,7 +65,7 @@ export default function Experiences() {
       <div className="place-self-center sm:w-2/3">
         {experiences.map((experience) => (
           <div
-            key={experience.title}
+            key={experience.header}
             className="grid gap-1 grid-cols-3 border-2 rounded-xl border-slate-500 py-6 px-3 my-12 my-2"
           >
             <div className="col-span-3 pb-4 w-2/3 grid justify-self-center items-center block lg:hidden">
@@ -79,9 +79,9 @@ export default function Experiences() {
               />
             </div>
             <div className="col-span-3 lg:col-span-2 pl-2">
-              <div className="text-2xl pb-1">{experience.title}</div>
+              <div className="text-2xl pb-1">{experience.header}</div>
               <div className="text-sm col-span-3 pb-2 text-slate-400">
-                {experience.technologies}
+                {experience.subheader}
               </div>
               <div>{experience.location}</div>
               <div>{experience.dates}</div>
