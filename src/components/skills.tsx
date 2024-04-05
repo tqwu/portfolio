73f01c76c6d8f60531dde086e4e5f5ff7aca67b1
Teresa <tqwu@ucsc.edu>
2024-04-05 14:22:31 -0700

    #8 Adjusted xl screen size responsiveness for skills section

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -3,7 +3,7 @@ export default function Skills() {
     <div className="text-slate-200 font-mono pt-16 pb-6 grid place-items-center gap-3">
       <div className="text-5xl place-self-center pb-2">Skills</div>
 
-      <div className="grid gap-3 w-4/5 xl:w-2/3 pl-0 md:pl-24">
+      <div className="grid gap-3 w-4/5 xl:w-2/3 2xl:w-1/2 pl-0 md:pl-24">
         <div>
           <span className="text-xl sm:text-3xl">Languages: </span>
           <span className="text-sm sm:text-base">
