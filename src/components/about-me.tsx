7c17d72383c81bd6454b594cb2c63e22fb5ed952
Teresa <tqwu@ucsc.edu>
2024-08-20 23:06:31 -0700

    Updating mobile view: about-me bio

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -8,13 +8,13 @@ export default function AboutMe() {
         {/* Mobile text start */}
         <div className="block sm:hidden pt-4 px-3 text-center">
           <div className="pb-2">
-            I&apos;m Teresa, a senior at the University of California, Santa
-            Cruz, studying Computer Science and History of Consciousness.
+            I&apos;m Teresa, a recent graduate from the University of California, Santa
+            Cruz, where I studied Computer Science and History of Consciousness.
           </div>
           <div className="pb-2">
-            I&apos;m currently working on an advanced memory utilization
-            analytics project for Nutanix to gain better insight into their
-            cloud infastructure memory usage.
+            I most recently worked on an advanced memory utilization
+            analytics system for Nutanix to gain better insight into their cloud
+            infrastructure memory usage.
           </div>
         </div>
         {/* Mobile text end */}
@@ -35,7 +35,7 @@ export default function AboutMe() {
           <div>
             I most recently worked on an advanced memory utilization
             analytics system for Nutanix to gain better insight into their cloud
-            infastructure memory usage.
+            infrastructure memory usage.
           </div>
           <div>
             I aspire to continue to build efficient applications that provide
