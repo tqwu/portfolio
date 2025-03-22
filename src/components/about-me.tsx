d3584d7979469d390b0c249d737a58dc5fc2d9cd
Teresa <tqwu@ucsc.edu>
2025-03-21 22:48:08 -0700

    Adding fade in framer transition

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import profile from "../../public/profile.png";
+import * as motion from "motion/react-client";
 
 export default function AboutMe() {
   return (
@@ -7,16 +8,23 @@ export default function AboutMe() {
       <div className="grid sm:grid-cols-7 sm:grid-rows-1 max-w-5xl text-base font-normal leading-7 text-slate-100">
         {/* Mobile text start */}
         <div className="block sm:hidden pt-4 px-3 text-center">
-          <div className="pb-2">
-            I&apos;m Teresa, a recent graduate from the University of
-            California, Santa Cruz, where I studied Computer Science and History
-            of Consciousness.
-          </div>
-          <div className="pb-2">
-            I most recently worked on an advanced memory utilization analytics
-            system for Nutanix to gain better insight into their cloud
-            infrastructure memory usage.
-          </div>
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1.5, ease: "easeOut" }}
+          >
+            <div className="pb-2">
+              I&apos;m Teresa, a recent graduate from the University of
+              California, Santa Cruz, where I studied Computer Science and
+              History of Consciousness.
+            </div>
+            <div className="pb-2">
+              I most recently worked on an advanced memory utilization analytics
+              system for Nutanix to gain better insight into their cloud
+              infrastructure memory usage.
+            </div>
+          </motion.div>
         </div>
         {/* Mobile text end */}
 
@@ -29,39 +37,57 @@ export default function AboutMe() {
 
         {/* Desktop paragraph start */}
         <div className="hidden sm:grid col-span-4 gap-5 pt-2 pr-7 sm:pl-7 md:pl-0">
-          <div>
-            I&apos;m Teresa, a recent graduate from the University of
-            California, Santa Cruz, where I studied Computer Science and History
-            of Consciousness.
-          </div>
-          <div>
-            I most recently worked on an advanced memory utilization analytics
-            system for Nutanix to gain better insight into their cloud
-            infrastructure memory usage.
-          </div>
-          <div>
-            I aspire to continue to build efficient applications that provide
-            optimal user experiences.
-          </div>
-          <div>
-            Outside of my technical studies, I enjoy hobbies that exercise
-            creativity! I love trying new recipes, tending to novel plants, and
-            I am passionate about fashion design and crafting funky new clothes.
-          </div>
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.5, ease: "easeOut" }}
+          >
+            <div>
+              I&apos;m Teresa, a recent graduate from the University of
+              California, Santa Cruz, where I studied Computer Science and
+              History of Consciousness.
+            </div>
+            <br />
+            <div>
+              I most recently worked on an advanced memory utilization analytics
+              system for Nutanix to gain better insight into their cloud
+              infrastructure memory usage.
+            </div>
+            <br />
+            <div>
+              I aspire to continue to build efficient applications that provide
+              optimal user experiences.
+            </div>
+            <br />
+            <div>
+              Outside of my technical studies, I enjoy hobbies that exercise
+              creativity! I love trying new recipes, tending to novel plants,
+              and I am passionate about fashion design and crafting funky new
+              clothes.
+            </div>
+          </motion.div>
         </div>
         {/* Desktop paragraph end */}
 
         {/* Mobile text start */}
         <div className="block sm:hidden pb-3 pt-2 px-3 text-center">
-          <div className="pb-2">
-            I aspire to continue to build efficient applications that provide
-            optimal user experiences.
-          </div>
-          <div>
-            Outside of my technical studies, I enjoy hobbies that exercise
-            creativity! I love trying new recipes, tending to novel plants, and
-            I am passionate about fashion design and crafting funky new clothes.
-          </div>
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1.5, ease: "easeOut" }}
+          >
+            <div className="pb-2">
+              I aspire to continue to build efficient applications that provide
+              optimal user experiences.
+            </div>
+            <div>
+              Outside of my technical studies, I enjoy hobbies that exercise
+              creativity! I love trying new recipes, tending to novel plants,
+              and I am passionate about fashion design and crafting funky new
+              clothes.
+            </div>
+          </motion.div>
         </div>
         {/* Mobile text end */}
       </div>
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,31 +1,40 @@
+import * as motion from "motion/react-client";
+
 export default function Skills() {
   return (
-    <div className="text-slate-200 font-mono pt-16 pb-6 grid place-items-center gap-3">
-      <div className="text-5xl place-self-center pb-2">Skills</div>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 1.5, ease: "easeOut" }}
+    >
+      <div className="text-slate-200 font-mono pt-16 pb-6 grid place-items-center gap-3">
+        <div className="text-5xl place-self-center pb-2">Skills</div>
 
-      <div className="grid gap-3 w-4/5 xl:w-2/3 2xl:w-1/2 pl-0 md:pl-24">
-        <div>
-          <span className="text-xl sm:text-3xl">Languages: </span>
-          <span className="text-sm sm:text-base">
-            Python, TypeScript/JavaScript, C, SQL
-          </span>
-        </div>
+        <div className="grid gap-3 w-4/5 xl:w-2/3 2xl:w-1/2 pl-0 md:pl-24">
+          <div>
+            <span className="text-xl sm:text-3xl">Languages: </span>
+            <span className="text-sm sm:text-base">
+              Python, TypeScript/JavaScript, C, SQL
+            </span>
+          </div>
 
-        <div>
-          <span className="text-xl sm:text-3xl">Technologies: </span>
-          <span className="text-sm sm:text-base">
-            React, Node.js, Next.js, Express.js, Vercel, PostgreSQL, Docker,
-            Prometheus, Grafana, Git
-          </span>
-        </div>
+          <div>
+            <span className="text-xl sm:text-3xl">Technologies: </span>
+            <span className="text-sm sm:text-base">
+              React, Node.js, Next.js, Express.js, Vercel, PostgreSQL, Docker,
+              Prometheus, Grafana, Git
+            </span>
+          </div>
 
-        <div>
-          <span className="text-xl sm:text-3xl">Knowledge: </span>
-          <span className="text-sm sm:text-base">
-            Agile/Scrum Methodologies, System Design, REST, GraphQL
-          </span>
+          <div>
+            <span className="text-xl sm:text-3xl">Knowledge: </span>
+            <span className="text-sm sm:text-base">
+              Agile/Scrum Methodologies, System Design, REST, GraphQL
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
