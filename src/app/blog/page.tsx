0a2cddbd976e195cca5ad5a1bb63c450c14b161c
Teresa <tqwu@ucsc.edu>
2025-04-15 20:15:24 -0700

    Adding animation for landing page of blog and plant page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,32 +1,40 @@
 import Image from "next/image";
 import HeaderBlog from "@/components/headerBlog";
 import FooterBlog from "@/components/footerBlog";
+import * as motion from "motion/react-client";
 
 export default function Blog() {
   return (
     <>
       <HeaderBlog />
       <main className="bg-peach py-16">
-        <div className="grid grid-cols-2">
-          <div className="col-span-1 justify-self-end self-center mt-24">
-            <Image
-              className=""
-              src={"/blogMainSide.png"}
-              alt=""
-              width={450}
-              height={900}
-            />
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 1.5, ease: "easeOut" }}
+        >
+          <div className="grid grid-cols-2">
+            <div className="col-span-1 justify-self-end self-center mt-24">
+              <Image
+                className=""
+                src={"/blogMainSide.png"}
+                alt=""
+                width={450}
+                height={900}
+              />
+            </div>
+            <div className="col-span-1 justify-self-start self-center mb-24">
+              <Image
+                className=""
+                src={"/blogMainProfile.png"}
+                alt=""
+                width={450}
+                height={900}
+              />
+            </div>
           </div>
-          <div className="col-span-1 justify-self-start self-center mb-24">
-            <Image
-              className=""
-              src={"/blogMainProfile.png"}
-              alt=""
-              width={450}
-              height={900}
-            />
-          </div>
-        </div>
+        </motion.div>
       </main>
       <FooterBlog />
     </>
--- src/app/blog/plants/page.tsx
+++ src/app/blog/plants/page.tsx
@@ -2,6 +2,7 @@ import HeaderBlog from "@/components/headerBlog";
 import FooterBlog from "@/components/footerBlog";
 import Link from "next/link";
 import Card from "@/components/card";
+import * as motion from "motion/react-client";
 
 const plantCards = [
   {
@@ -32,43 +33,50 @@ export default function Plants() {
     <>
       <HeaderBlog />
       <main className="bg-lime-100 font-mono text-sm font-light">
-        <div>This is the Plants page.</div>
-        <div className="grid justify-items-center">
-          <div className="grid grid-cols-4 gap-48 justify-items-center w-3/4 py-12">
-            {plantCards.map((p) => (
-              <div key={p.endpoint} className="col-span-1">
-                <Card
-                  title={p.title}
-                  titleClickable={true}
-                  description={p.description}
-                  endpoint={p.endpoint}
-                  imageSrc={p.imageSrc}
-                  buttonText={p.buttonText}
-                />
-              </div>
-            ))}
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 1.5, ease: "easeOut" }}
+        >
+          <div>This is the Plants page.</div>
+          <div className="grid justify-items-center">
+            <div className="grid grid-cols-4 gap-48 justify-items-center w-3/4 py-12">
+              {plantCards.map((p) => (
+                <div key={p.endpoint} className="col-span-1">
+                  <Card
+                    title={p.title}
+                    titleClickable={true}
+                    description={p.description}
+                    endpoint={p.endpoint}
+                    imageSrc={p.imageSrc}
+                    buttonText={p.buttonText}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-        <div>
-          <div>Some helpful / interesting resources about plants</div>
-          <Link
-            href="https://pollinator.art/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-600 hover:text-blue-300"
-          >
-            Pollinator Pathmaker
-          </Link>
-          <br />
-          <Link
-            href="https://www.youtube.com/@tannertheplanter102"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-600 hover:text-blue-300"
-          >
-            Tanner the Planter
-          </Link>
-        </div>
+          <div>
+            <div>Some helpful / interesting resources about plants</div>
+            <Link
+              href="https://pollinator.art/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:text-blue-300"
+            >
+              Pollinator Pathmaker
+            </Link>
+            <br />
+            <Link
+              href="https://www.youtube.com/@tannertheplanter102"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:text-blue-300"
+            >
+              Tanner the Planter
+            </Link>
+          </div>
+        </motion.div>
       </main>
       <FooterBlog />
     </>
