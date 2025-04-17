613eca52517d09f59f32ec1a5aeffc638484d08b
Teresa <tqwu@ucsc.edu>
2025-04-16 20:54:39 -0700

    Fixing key error and adjusting responsiveness of 2xl screen

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -55,74 +55,73 @@ export default function Projects() {
 
         <div className="grid place-self-center place-items-center sm:w-4/5">
           {projects.map((project) => (
-            <motion.div
-              whileHover={{
-                scale: [null, 1.05],
-                transition: {
-                  duration: 0.3,
-                  ease: ["easeInOut", "easeOut"],
-                },
-              }}
-            >
-              <div
-                key={project.title}
-                className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-1 2xl:w-3/4 border-2 rounded-xl border-slate-500 py-6 px-3 my-12 my-2 2xl:px-12"
+            <div key={project.title} className="w-full 2xl:w-3/4">
+              <motion.div
+                whileHover={{
+                  scale: [null, 1.05],
+                  transition: {
+                    duration: 0.3,
+                    ease: ["easeInOut", "easeOut"],
+                  },
+                }}
               >
-                {/* Project Description Begin */}
-                <div className="place-self-center col-span-1 pl-2 max-w-2xl xl:justify-self-end">
-                  <div className="text-2xl pb-2">{project.title}</div>
-                  <div className="text-sm col-span-3 pb-2 text-slate-400">
-                    {project.technologies}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-1 border-2 rounded-xl border-slate-500 py-6 px-3 my-2 2xl:px-12">
+                  {/* Project Description Begin */}
+                  <div className="place-self-center col-span-1 pl-2 max-w-2xl xl:justify-self-end">
+                    <div className="text-2xl pb-2">{project.title}</div>
+                    <div className="text-sm col-span-3 pb-2 text-slate-400">
+                      {project.technologies}
+                    </div>
+                    <ul className="text-sm pt-2 col-span-3">
+                      {project.description.map((item, index) => (
+                        <li key={index} className="py-1.5">
+                          {item}
+                        </li>
+                      ))}
+                    </ul>
+                    <div className="grid w-full place-items-center">
+                      <a
+                        href={project.webpage}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <button className="flex items-center gap-2 bg-slate-400 hover:bg-slate-300 text-navy font-normal mt-4 py-2 px-4 rounded-full border border-white">
+                          {project.webpageDescription}
+                          {project.webpageIcon && (
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={3}
+                              stroke="currentColor"
+                              className="w-4 h-4"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                              />
+                            </svg>
+                          )}
+                        </button>
+                      </a>
+                    </div>
                   </div>
-                  <ul className="text-sm pt-2 col-span-3">
-                    {project.description.map((item, index) => (
-                      <li key={index} className="py-1.5">
-                        {item}
-                      </li>
-                    ))}
-                  </ul>
-                  <div className="grid w-full place-items-center">
-                    <a
-                      href={project.webpage}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <button className="flex items-center gap-2 bg-slate-400 hover:bg-slate-300 text-navy font-normal mt-4 py-2 px-4 rounded-full border border-white">
-                        {project.webpageDescription}
-                        {project.webpageIcon && (
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={3}
-                            stroke="currentColor"
-                            className="w-4 h-4"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-                            />
-                          </svg>
-                        )}
-                      </button>
-                    </a>
-                  </div>
-                </div>
-                {/* Project Description End */}
+                  {/* Project Description End */}
 
-                <div className="col-span-1 place-self-center p-1.5 md:ml-6 xl:justify-self-center">
-                  <Image
-                    className="rounded-xl"
-                    src={project.preview}
-                    height={600}
-                    width={500}
-                    alt={`${project.title} Preview`}
-                    priority
-                  />
+                  <div className="col-span-1 place-self-center p-1.5 md:ml-6 xl:justify-self-center">
+                    <Image
+                      className="rounded-xl"
+                      src={project.preview}
+                      height={600}
+                      width={500}
+                      alt={`${project.title} Preview`}
+                      priority
+                    />
+                  </div>
                 </div>
-              </div>
-            </motion.div>
+              </motion.div>
+            </div>
           ))}
         </div>
       </div>
